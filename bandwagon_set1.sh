ab0f78e1f6226d60f07e1cfe3daf8e0e85639ab7
LukeLu0415 <lldyouxiang@163.com>
2019-08-08 12:21:04 +0800

    Update bandwagon_set1.sh

--- bandwagon_set1.sh
+++ bandwagon_set1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget ¡ª no-check-certificate -O shadowsocks.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks.sh
+wget - no-check-certificate -O shadowsocks.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks.sh
 chmod +x shadowsocks.sh
